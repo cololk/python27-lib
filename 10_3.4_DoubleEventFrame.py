ac6766486488a632a6a13e6d3552122998185322
cololk <leoricee@gmail.com>
2017-03-22 00:33:56 +0800

    更新敘述

--- 10_3.4_DoubleEventFrame.py
+++ 10_3.4_DoubleEventFrame.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 #-*- coding:utf-8 -*-
-
 import wx
 
 class DoubleEventFrame(wx.Frame):
@@ -8,8 +7,8 @@ class DoubleEventFrame(wx.Frame):
 		wx.Frame.__init__(self,parent,id,'Frame With Button',size=(300,100))
 		self.panel=wx.Panel(self,-1)
 		self.button=wx.Button(self.panel,-1,"Click Me",pos=(100,15))
-		self.Bind(wx.EVT_BUTTON, self.OnButtonClick,self.button)  #綁定按鈕敲擊事件
-		self.button.Bind(wx.EVT_LEFT_DOWN, self.OnMouseDown)  #綁定鼠標左鍵按下事件
+		self.Bind(wx.EVT_BUTTON, self.OnButtonClick,self.button)  #1 綁定按鈕敲擊事件
+		self.button.Bind(wx.EVT_LEFT_DOWN, self.OnMouseDown)  #2 綁定鼠標左鍵按下事件
 
 	def OnButtonClick(self,event): #這個就是按鈕事件的處理器
 		self.panel.SetBackgroundColour('Green')
@@ -17,7 +16,7 @@ class DoubleEventFrame(wx.Frame):
 
 	def OnMouseDown(self,event):  #這個就是滑鼠左鍵點擊事件的處理器
 		self.button.SetLabel("Again!")
-		event.Skip()
+		event.Skip() #為了進一步的事件處理,必須調用Skip()方法,否則進一步的事件處理將被阻止(按鈕放開才會觸發EVT_BUTTON事件)
 
 if __name__=='__main__':
 	app=wx.PySimpleApp()
